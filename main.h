e0eeecfe53804b284033911a2cab1521331d275b
phitabs24 <107364663+phitabs24@users.noreply.github.com>
2022-10-19 03:07:47 +0100

    task 0 to print to standard output

--- main.h
+++ main.h
@@ -32,5 +32,8 @@ typedef struct printHandler
 
 /* print_nums */
 int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
 
 #endif
